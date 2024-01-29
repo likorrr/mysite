bb3f13809991192433658d383c14fcfb3af68b71
likorrr <148974253+likorrr@users.noreply.github.com>
2024-01-29 22:57:08 +0300

    Add files via upload

---
+++ translator_project/main/views.py
@@ -0,0 +1,13 @@
+from django.shortcuts import render
+from googletrans import Translator
+def index(request):
+    if request.method == 'POST':
+        lang = request.POST.get('lang', None)
+        txt = request.POST.get('txt', None)
+
+        translator = Translator()
+        tr = translator.translate(txt, dest=lang)
+
+        return render(request, 'main/index.html', {'result': tr.text})
+    return render(request, 'main/index.html')
+        
