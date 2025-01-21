9df1a0435ae4e2aa00f4caec3ebd03c9e31af260
likorrr <148974253+likorrr@users.noreply.github.com>
2025-01-21 19:14:14 +0300

    views.py

--- translator_project/main/views.py
+++ translator_project/main/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
 from googletrans import Translator
+import asyncio
+
+async def translate_text(txt, dest):
+    async with Translator() as translator:
+        result = await translator.translate(txt, dest=dest)
+        return result
+
 def index(request):
     if request.method == 'POST':
         lang = request.POST.get('lang', None)
         txt = request.POST.get('txt', None)
 
         translator = Translator()
-        tr = translator.translate(txt, dest=lang)
+        result = asyncio.run(translate_text(txt, lang))
 
-        return render(request, 'main/index.html', {'result': tr.text})
+        return render(request, 'main/index.html', {'result': result.text, 'lang':lang})
     return render(request, 'main/index.html')
+        
         
